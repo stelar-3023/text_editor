20ccddc5e5e49612e8d985f35c82f8962bc70fdf
Steve Larsen <slarsen-3@att.net>
2021-01-30 17:54:16 -0600

    added classic build to EditorComponent.js

---
+++ src/components/EditorComponent.js
@@ -0,0 +1,36 @@
+import React, {Component} from "react";
+import {CKEditor} from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+
+
+
+
+class Editor extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h2>React Text Editor</h2>
+                <CKEditor
+                    editor={ClassicEditor}
+                    data="<p>Thanks for choosing my editor</p>"
+                    onReady={editor => {
+                        // Store the editor and use it when it is needed
+                        console.log("Editor is ready to use!", editor);
+                    }}
+                    onChange={(event, editor) => {
+                        const data = editor.getData();
+                        console.log({event, editor, data});
+                    }}
+                    onBlur={(event, editor) => {
+                        console.log("Blur", editor);
+                    }}
+                    onFocus={(event, editor) => {
+                        console.log("Focus", editor)
+                    }}
+                />
+            </div>
+        )
+    }
+}
+
+export default Editor;
